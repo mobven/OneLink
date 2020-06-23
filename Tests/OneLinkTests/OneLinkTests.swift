1068d12a06a04505297da623e02c9212a51b3c58
erenbayrakmobven <63582982+erenbayrakmobven@users.noreply.github.com>
2020-06-23 12:26:09 +0300

    test fixes

--- Tests/OneLinkTests/OneLinkTests.swift
+++ Tests/OneLinkTests/OneLinkTests.swift
@@ -14,7 +14,7 @@ class OneLinkTests: XCTestCase {
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-//        MobKit.setup(with: [OneLinkSpy.sharedSpy])
+        OneLinkSpy.sharedSpy.setup()
     }
     
     func testImmediateLink() {
